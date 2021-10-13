0c9d2dad144405273e7a1226a706248fa02f6b87
1Hasi <hasolol136@gmail.com>
2021-10-13 15:53:57 +0200

    change 3

--- javascript.js
+++ javascript.js
@@ -1,2 +1,3 @@
-const sum = (a,b) => a + b ; console.log(sum(5, 3));
-
+const names = ['Hasan', 'Max', 'Dennis', 'Patrick', 'Phillip'];
+const hasMax = names.some((name) =>  name === 'Max' );
+console.log(hasMax);
